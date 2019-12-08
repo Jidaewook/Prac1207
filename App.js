dbbb8acb0209367a001c8ec0c3e2f4a2ed559466
Jay <psatdoctor@naver.com>
2019-12-08 09:46:17 +0900

    Routes 연결

--- App.js
+++ App.js
@@ -13,6 +13,10 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('mongodb connected..'))
     .catch(err => console.log(err));
 
+// 서버가 연결된 후, userRoutes의 페이지 내 주소로 연결될 수 있게 해주는 주소등록
+app.use("/user", userRoutes);
+app.use("/profile", profileRoutes);
+
 const port = 3000;
 
 app.listen(port, () => console.log(`Server Running on ${port}`));
